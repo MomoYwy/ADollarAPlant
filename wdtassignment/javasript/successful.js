71838e1122396e6dd65cf1ddb052e69c870fe164
Tim <138644162+Cilakak@users.noreply.github.com>
2024-02-18 15:00:22 +0800

    css and linking changes

--- wdtassignment/javasript/successful.js
+++ wdtassignment/javasript/successful.js
@@ -1,8 +1,11 @@
 function backHome() {
     // Redirect to the home page
     window.location.href = '/ADollarAPlant/wdtassignment/php/mainpage.php';
+<<<<<<< Updated upstream
 };
 
 function backToLogin() {
     window.location.href = '/ADollarAPlant/wdtassignment/php/login.php';
+=======
+>>>>>>> Stashed changes
 };
