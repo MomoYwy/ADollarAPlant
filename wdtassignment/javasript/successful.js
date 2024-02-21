f384ce2e3554063106542fad4c13ac375184b335
MomoYwy <138420874+MomoYwy@users.noreply.github.com>
2024-02-21 16:40:36 +0800

    do what every you guys want with the code

--- wdtassignment/javasript/successful.js
+++ wdtassignment/javasript/successful.js
@@ -1,11 +1,8 @@
 function backHome() {
     // Redirect to the home page
     window.location.href = '/ADollarAPlant/wdtassignment/php/mainpage.php';
-<<<<<<< Updated upstream
 };
 
 function backToLogin() {
     window.location.href = '/ADollarAPlant/wdtassignment/php/login.php';
-=======
->>>>>>> Stashed changes
 };
